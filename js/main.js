fe3ccf16c3eabb5e23b44b8007befb29ce47ba3f
eocampo9125 <36829968+eocampo9125@users.noreply.github.com>
2024-03-29 13:39:31 -0500

    testimonial
    testimonial

--- js/main.js
+++ js/main.js
@@ -75,6 +75,38 @@
 
     // Testimonial-carousel
    
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+
 
 
 
